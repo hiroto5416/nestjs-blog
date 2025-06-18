1a3a43b2b278e4cdffec0b5bb15f125bd9228f91
hiroto <kitta1213@gmail.com>
2025-06-19 06:01:49 +0900

    Prisma Clientの生成とNestJSへの統合

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -9,8 +11,9 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, //各モジュールで毎回 ConfigModule をインポートしなくても ConfigService を使えるよう
       envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
